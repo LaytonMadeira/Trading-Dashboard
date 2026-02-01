7368a10796c962b1066a2e989e0ccc61887111b6
Layto <layton.madeira@stonybrook.edu>
2026-02-01 01:30:04 -0500

    Buy signals added to track 50 day and 200 day moving averages for the golden cross

--- Stock Lookup.py
+++ Stock Lookup.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import yfinance as yf
-
+import pandas as pd
 
 st.write("## Welcome to the Trading Dashboard!")
 
@@ -14,14 +14,29 @@ with timeframe:
 df = None
 
 if inp:
-    st.write(f"Searching for value: {inp}")
+    st.write(f"Symbol Entered: {inp.upper()}")
     df = yf.download(f"{inp}", period=f"{1 if not timeframe else timeframe}y")
+
+    df.columns = df.columns.get_level_values(0)
 
     # display whether stock data was acquired
     st.write("\n")
     st.write(f"Data aquisition status: {not df.empty}")
-    # st.write()
+    buy_signals = st.toggle("Buy Signals", help="Displays 50 day and 200 day moving averages")
 
+    # calculate 50 day moving average and 200 day moving average and tell whether to buy or not 
+    if buy_signals:
+        df["MA50"] = df["Close"].rolling(window=50).mean()
+        df["MA200"] = df["Close"].rolling(window=200).mean()
+
+    plot_cols = ["Close"]
+
+    if buy_signals:
+        plot_cols += "MA50"
+        plot_cols += "MA200"
+        
     # data found
-    if not df.empty:
-        st.line_chart(df["Close"])
+    if not df.empty and buy_signals:
+        st.line_chart(df[plot_cols])
+
+
