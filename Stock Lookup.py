35d6d884d76ce5bebba708b0a06c245214f8104d
Layto <layton.madeira@stonybrook.edu>
2026-02-03 00:24:27 -0500

    Prototype 1 Complete, code refactored for modularity and professionalism

--- Stock Lookup.py
+++ Stock Lookup.py
@@ -3,9 +3,42 @@ import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
 
+# apply site-wide styles
 from styles import apply_css
-
 apply_css()
+
+@st.cache_data(show_spinner=False)
+def get_stock_data(ticker, length, length_units='y'):
+
+    try:
+        # check length input 
+        if not length or not length.isdigit():
+            length = '1y'
+        else:
+            length = f'{length}{length_units}'
+        #download stock data
+        df = yf.download(ticker, period=length)
+        df.attrs = {}
+        #download company data
+        stock = yf.Ticker(ticker)
+
+        
+        return df, stock.info
+
+    except:
+        return pd.DataFrame(), None
+
+@st.cache_data()
+def get_stock_name(stock_info, ticker):
+    return stock_info.get('longName', ticker.upper() if ticker else 'Company Not Found')
+
+def flatten_stock_data(stock_df):
+    # flattens multindex for easier indexing of single stock query
+    if isinstance(stock_df.columns, pd.MultiIndex):
+        stock_df.columns = df.columns.get_level_values(0)
+    
+    return stock_df
+
 
 st.write("## Welcome to the Trading Dashboard!")
 
@@ -17,82 +50,69 @@ with timeframe:
     timeframe = st.text_input("(Optional) Enter the period, in years, to examine:", placeholder = "1, 2, 3, etc. (integers)", autocomplete="off")   
 
 if inp:
-    st.write(f"Symbol Entered: {inp.upper()}")
-    try: 
-        df = yf.download(f"{inp}", period=f"{1 if (not timeframe or not timeframe.isdigit()) else timeframe}y")
-        df.columns = df.columns.get_level_values(0) #flatten to remove multindex (not needed for single stock query)
-        stock = yf.Ticker(inp)
-        stock_name = stock.info.get('longName', 'Company Name Not Found')
+    inp = inp.upper()
+    with st.spinner(f"Searching for {inp}..."):
+        df, stock_info = get_stock_data(inp, timeframe, 'y')
+        # need to flatten here
+        df = flatten_stock_data(df)
+        stock_name = get_stock_name(stock_info, inp)
 
-    except:
-        st.write("Error fetching results, please check your connection and try again later.")
-        st.stop()
-
-
-    st.write("\n")
-    
-    # Error check - incorrect ticker
+    # data was not found
     if df.empty:
-        st.write(f"Error: Data not found for {inp.upper()}. Please double check ticker symbol and try again.")
+        st.write(f"ERROR: Data not found for {inp}. Please check the ticker.")
         st.stop()
+    
+    # found data
+    # st.success(f"Data found for {inp}")
 
-    buy_signals = st.toggle("Moving Averages", help="Displays 50 day and 200 day moving averages by default, helping investors find golden crosses.")
-    plot_cols = ["Close"]
+    # container for graph
+    with st.container():
+        buy_signals = st.toggle(
+            "Moving Averages", 
+            help="Moving Averages help investors find Golden Crosses.")
+        plot_cols = ["Close"]
+        # default moving averages
+        ma_one_val = 50
+        ma_two_val = 200
+        # user wants to view moving averages
+        if buy_signals:
+            ma_one_col, ma_two_col = st.columns(2)
+            with ma_one_col:
+                ma_one_val = st.number_input("First MA", min_value=1, value=50)
+            with ma_two_col:
+                ma_two_val = st.number_input("Second MA", min_value=1, value=200)
+            #calc moving averages
+            df["Ma_One"] = df["Close"].rolling(window=ma_one_val).mean()
+            df["Ma_Two"] = df["Close"].rolling(window=ma_two_val).mean()
+            plot_cols+=["Ma_One", "Ma_Two"]
 
-    # calculate 50 day moving average and 200 day moving average and tell whether to buy or not 
-    if buy_signals:
-        ma_one, ma_two = st.columns(2)
-        with ma_one:
-            ma_one = st.number_input("Moving Average One:", min_value=1, value=50)
-        with ma_two:
-            ma_two = st.number_input("Moving Average Two:", min_value=1, value=200)
-
-
-        df["MA_ONE"] = df["Close"].rolling(window=50 if not ma_one else ma_one).mean()
-        df["MA_TWO"] = df["Close"].rolling(window=200).mean()
-
-        plot_cols += ["MA_ONE", "MA_TWO"]
-
-    # data found
-    if not df.empty:
-        # plot data
         figure = go.Figure()
 
-        # Main Line (WHITE)
-        figure.add_trace(
-            go.Scatter(
-                x=df.index,
-                y=df["Close"],
-                mode="lines",
-                name="Price",
-                line=dict(color="white", width=2)
-            )
-        )
+        figure.add_trace(go.Scatter(
+            x=df.index,
+            y=df["Close"],
+            mode="lines",
+            name="Price",
+            line=dict(color='white', width=2)
+        ) )
+        
+        if buy_signals:
+            figure.add_trace(go.Scatter(
+            x=df.index, 
+            y=df["Ma_One"], 
+            mode="lines", 
+            name=f"{ma_one_val} Day MA", 
+            line=dict(color="#FF9B05", width=1.5)
+            ))
+            
+            figure.add_trace(go.Scatter(
+            x=df.index, 
+            y=df["Ma_Two"], 
+            mode="lines", 
+            name=f"{ma_two_val} Day MA", 
+            line=dict(color="#4b63ff", width=1.5)
+            ))
 
-        if buy_signals: 
-            # MA50 (RED)
-            figure.add_trace(
-                go.Scatter(
-                    x=df.index,
-                    y=df["MA_ONE"],
-                    mode="lines",
-                    name="First MA",
-                    line=dict(color="red", width=2)
-                )
-            )
-
-            # MA_TWO (BLUE)
-            figure.add_trace(
-                go.Scatter(
-                    x=df.index,
-                    y=df["MA_TWO"],
-                    mode="lines",
-                    name="Second MA",
-                    line=dict(color="blue", width=2)
-                )
-            )
-
-        #Display graph
         figure.update_layout(
             title={
                 'text': f'Price Action for {stock_name}',
@@ -104,7 +124,98 @@ if inp:
             xaxis_title="Date",
             yaxis_title="Price (USD)",
             hovermode="x unified",
+            height=600
         )
 
         st.plotly_chart(figure, use_container_width=True)
+# if inp:
+#     st.write(f"Symbol Entered: {inp.upper()}")
+#     try: 
+#         df = yf.download(f"{inp}", period=f"{1 if (not timeframe or not timeframe.isdigit()) else timeframe}y")
+#         df.columns = df.columns.get_level_values(0) #flatten to remove multindex (not needed for single stock query)
+#         stock = yf.Ticker(inp)
+#         stock_name = stock.info.get('longName', 'Company Name Not Found')
+
+#     except:
+#         st.write("Error fetching results, please check your connection and try again later.")
+#         st.stop()
+
+#     st.write("\n")
+    
+#     # Error check - incorrect ticker
+#     if df.empty:
+#         st.write(f"Error: Data not found for {inp.upper()}. Please double check ticker symbol and try again.")
+#         st.stop()
+
+#     buy_signals = st.toggle("Moving Averages", help="Displays 50 day and 200 day moving averages by default, helping investors find golden crosses.")
+#     plot_cols = ["Close"]
+
+#     # calculate 50 day moving average and 200 day moving average and tell whether to buy or not 
+#     if buy_signals:
+#         ma_one, ma_two = st.columns(2)
+#         with ma_one:
+#             ma_one = st.number_input("Moving Average One:", min_value=1, value=50)
+#         with ma_two:
+#             ma_two = st.number_input("Moving Average Two:", min_value=1, value=200)
+
+
+#         df["MA_ONE"] = df["Close"].rolling(window=ma_one if ma_one else 50).mean()
+#         df["MA_TWO"] = df["Close"].rolling(window=ma_two if ma_two else 200).mean()
+
+#         plot_cols += ["MA_ONE", "MA_TWO"]
+
+#     # data found
+#     if not df.empty:
+#         # plot data
+#         figure = go.Figure()
+
+#         # Main Line (WHITE)
+#         figure.add_trace(
+#             go.Scatter(
+#                 x=df.index,
+#                 y=df["Close"],
+#                 mode="lines",
+#                 name="Price",
+#                 line=dict(color="white", width=2)
+#             )
+#         )
+
+#         if buy_signals: 
+#             # MA50 (RED)
+#             figure.add_trace(
+#                 go.Scatter(
+#                     x=df.index,
+#                     y=df["MA_ONE"],
+#                     mode="lines",
+#                     name="First MA",
+#                     line=dict(color="red", width=2)
+#                 )
+#             )
+
+#             # MA_TWO (BLUE)
+#             figure.add_trace(
+#                 go.Scatter(
+#                     x=df.index,
+#                     y=df["MA_TWO"],
+#                     mode="lines",
+#                     name="Second MA",
+#                     line=dict(color="blue", width=2)
+#                 )
+#             )
+
+#         #Display graph
+#         figure.update_layout(
+#             title={
+#                 'text': f'Price Action for {stock_name}',
+#                 'y': 0.9,
+#                 'x': 0.5,
+#                 'xanchor': 'center', 
+#                 'yanchor': 'top'
+#                 },
+#             xaxis_title="Date",
+#             yaxis_title="Price (USD)",
+#             hovermode="x unified",
+#         )
+
+#         st.plotly_chart(figure, use_container_width=True)
 
