354265b813dfe3062da80b75b40cfc47c36b0320
Layto <layton.madeira@stonybrook.edu>
2026-02-03 00:24:33 -0500

    Prototype 1 Complete, code refactored for modularity and professionalism

--- Stock Lookup.py
+++ Stock Lookup.py
@@ -9,7 +9,6 @@ apply_css()
 
 @st.cache_data(show_spinner=False)
 def get_stock_data(ticker, length, length_units='y'):
-
     try:
         # check length input 
         if not length or not length.isdigit():
@@ -21,10 +20,7 @@ def get_stock_data(ticker, length, length_units='y'):
         df.attrs = {}
         #download company data
         stock = yf.Ticker(ticker)
-
-        
         return df, stock.info
-
     except:
         return pd.DataFrame(), None
 
@@ -38,7 +34,6 @@ def flatten_stock_data(stock_df):
         stock_df.columns = df.columns.get_level_values(0)
     
     return stock_df
-
 
 st.write("## Welcome to the Trading Dashboard!")
 
@@ -61,9 +56,6 @@ if inp:
     if df.empty:
         st.write(f"ERROR: Data not found for {inp}. Please check the ticker.")
         st.stop()
-    
-    # found data
-    # st.success(f"Data found for {inp}")
 
     # container for graph
     with st.container():
@@ -128,94 +120,3 @@ if inp:
         )
 
         st.plotly_chart(figure, use_container_width=True)
-# if inp:
-#     st.write(f"Symbol Entered: {inp.upper()}")
-#     try: 
-#         df = yf.download(f"{inp}", period=f"{1 if (not timeframe or not timeframe.isdigit()) else timeframe}y")
-#         df.columns = df.columns.get_level_values(0) #flatten to remove multindex (not needed for single stock query)
-#         stock = yf.Ticker(inp)
-#         stock_name = stock.info.get('longName', 'Company Name Not Found')
-
-#     except:
-#         st.write("Error fetching results, please check your connection and try again later.")
-#         st.stop()
-
-#     st.write("\n")
-    
-#     # Error check - incorrect ticker
-#     if df.empty:
-#         st.write(f"Error: Data not found for {inp.upper()}. Please double check ticker symbol and try again.")
-#         st.stop()
-
-#     buy_signals = st.toggle("Moving Averages", help="Displays 50 day and 200 day moving averages by default, helping investors find golden crosses.")
-#     plot_cols = ["Close"]
-
-#     # calculate 50 day moving average and 200 day moving average and tell whether to buy or not 
-#     if buy_signals:
-#         ma_one, ma_two = st.columns(2)
-#         with ma_one:
-#             ma_one = st.number_input("Moving Average One:", min_value=1, value=50)
-#         with ma_two:
-#             ma_two = st.number_input("Moving Average Two:", min_value=1, value=200)
-
-
-#         df["MA_ONE"] = df["Close"].rolling(window=ma_one if ma_one else 50).mean()
-#         df["MA_TWO"] = df["Close"].rolling(window=ma_two if ma_two else 200).mean()
-
-#         plot_cols += ["MA_ONE", "MA_TWO"]
-
-#     # data found
-#     if not df.empty:
-#         # plot data
-#         figure = go.Figure()
-
-#         # Main Line (WHITE)
-#         figure.add_trace(
-#             go.Scatter(
-#                 x=df.index,
-#                 y=df["Close"],
-#                 mode="lines",
-#                 name="Price",
-#                 line=dict(color="white", width=2)
-#             )
-#         )
-
-#         if buy_signals: 
-#             # MA50 (RED)
-#             figure.add_trace(
-#                 go.Scatter(
-#                     x=df.index,
-#                     y=df["MA_ONE"],
-#                     mode="lines",
-#                     name="First MA",
-#                     line=dict(color="red", width=2)
-#                 )
-#             )
-
-#             # MA_TWO (BLUE)
-#             figure.add_trace(
-#                 go.Scatter(
-#                     x=df.index,
-#                     y=df["MA_TWO"],
-#                     mode="lines",
-#                     name="Second MA",
-#                     line=dict(color="blue", width=2)
-#                 )
-#             )
-
-#         #Display graph
-#         figure.update_layout(
-#             title={
-#                 'text': f'Price Action for {stock_name}',
-#                 'y': 0.9,
-#                 'x': 0.5,
-#                 'xanchor': 'center', 
-#                 'yanchor': 'top'
-#                 },
-#             xaxis_title="Date",
-#             yaxis_title="Price (USD)",
-#             hovermode="x unified",
-#         )
-
-#         st.plotly_chart(figure, use_container_width=True)
-
